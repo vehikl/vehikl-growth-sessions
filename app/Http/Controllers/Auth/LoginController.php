b476d1b5afdb4beaae355299a45ea89b5f3bc788
Felipe Flor <f.flor@vehikl.com>
2020-05-19 11:46:35 -0400

    Allow users to login/register using Github OAuth

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -4,7 +4,10 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
+use App\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 use Laravel\Socialite\Facades\Socialite;
 
 class LoginController extends Controller
@@ -26,6 +29,17 @@ class LoginController extends Controller
 
     public function handleProviderCallback()
     {
-        $user = Socialite::driver('github')->user();
+        $githubUser = Socialite::driver('github')->user();
+        $socialMobUser = User::query()->where('email', $githubUser->email)->first();
+        if (! $socialMobUser) {
+            $socialMobUser = User::query()->create([
+                'name' => $githubUser->getEmail(),
+                'email' => $githubUser->getEmail(),
+                'avatar' => $githubUser->getAvatar(),
+                'password' => Hash::make(Str::random()),
+            ]);
+        }
+        auth()->login($socialMobUser);
+        return redirect($this->redirectPath());
     }
 }
