0f43913134c57c4a2300e4600235daaf700d56ca
Felipe Flor <f.flor@vehikl.com>
2020-05-19 22:49:14 -0400

    Add owner and members to the socialmob payload

--- app/SocialMob.php
+++ app/SocialMob.php
@@ -7,11 +7,18 @@ use Illuminate\Database\Eloquent\Model;
 
 class SocialMob extends Model
 {
+    protected $with = ['owner', 'users'];
+
     protected $fillable = [
         'topic',
         'start_time',
         'owner_id'
     ];
+
+    public function owner()
+    {
+        return $this->belongsTo(User::class, 'owner_id');
+    }
 
     public function users()
     {
