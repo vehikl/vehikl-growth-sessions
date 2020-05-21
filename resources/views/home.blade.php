ea3647538363addd003eaf6bde30cb972180d04e
Felipe Flor <f.flor@vehikl.com>
2020-05-21 11:26:09 -0400

    Fix typo

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,8 +1,6 @@
 @extends('layouts.app')
 
 @section('content')
-    @guest
-        @include('partials.about')
-    @endguest
+    @include('partials.about')
     <week-view class="mt-6" :user="{{ json_encode(auth()->user()) }}"></week-view>
 @endsection
