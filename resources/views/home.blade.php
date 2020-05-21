96a049444dd3d6caab5b93470e8c3ab8d9f86c45
Felipe Flor <f.flor@vehikl.com>
2020-05-21 15:07:36 -0400

    Add login reminder to home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -2,6 +2,11 @@
 
 @section('content')
     @include('partials.about')
+    @guest
+        <p role="alert" class="block text-center text-orange-600 text-2xl">To join existing mobs or create your own, you must
+            <strong><a href="{{route('oauth.login.redirect')}}" class="underline hover:text-orange-400">log in</a></strong>!
+        </p>
+    @endguest
     <week-view class="mt-6" :user="{{ json_encode(auth()->user()) }}"></week-view>
     <a class="inline-block bg-blue-600 hover:bg-blue-700 focus:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2 my-10"
        target="_blank"
