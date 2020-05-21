14f9635df0f3db564e0b75db7e4002b13b46546c
Felipe Flor <f.flor@vehikl.com>
2020-05-21 12:58:32 -0400

    Update caption

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,6 +6,6 @@
     <a class="inline-block bg-blue-600 hover:bg-blue-700 focus:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2 my-10"
        target="_blank"
        href="https://github.com/FRFlor/social-mob/issues">
-        Have suggestions? Please, share them here!
+        Have suggestions for this app? Please, share them here!
     </a>
 @endsection
