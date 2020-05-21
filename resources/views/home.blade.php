39c4535a4df99b2f5a879e8444a3109ec628602b
Grant Lovell <grant.lovell@gmail.com>
2020-05-21 07:57:25 -0400

    Add about social mob

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
+    <div>
+        <h2>What is social mob?</h2>
+
+        <p>In "The 7 Habits of Highly Effective People" Stephen Covey tells the story of a hiker who was walking through a forest when they came across a frustrated lumberjack.</p>
+
+        <p>The lumberjack was trying to cut down a tree with and was swearing and cursing labouring in vain.</p>
+
+        <p>“What’s the problem?” The hiker asked.</p>
+
+        <p>“My saw’s blunt and won’t cut the tree properly.”  The lumberjack responded.</p>
+
+        <p>“Why don’t you just sharpen it?”</p>
+
+        <p>“Because then I would have to stop sawing.”  Said the lumberjack.</p>
+
+        <p>“But if you sharpened your saw, you could cut more efficiently and effectively than before.”</p>
+
+        <p>“But I don’t have time to stop!” The lumberjack retorted, getting even more frustrated.</p>
+
+        <p>The hiker shook his head and kept on walking, leaving the lumberjack to his pointless frustration.</p>
+
+        <p>Social Mob is an oppotunity for each of us to sharpen our saw (and discover brand new innovations like the chainsaw!). It is a space for experimenting, getting help when we are stuck, learning from others and sharing the benefits of what we have discovered in our travels.</p>
+    </div>
+
     <week-view class="mt-6" :user="{{ json_encode(auth()->user()) }}"></week-view>
 @endsection
