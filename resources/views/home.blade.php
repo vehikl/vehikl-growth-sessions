d13c0bd63d99d7b98fb05d8edc3286aa3b8a2e5c
Felipe Flor <f.flor@vehikl.com>
2020-05-21 12:56:08 -0400

    Add link to issues page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,4 +3,9 @@
 @section('content')
     @include('partials.about')
     <week-view class="mt-6" :user="{{ json_encode(auth()->user()) }}"></week-view>
+    <a class="inline-block bg-blue-600 hover:bg-blue-700 focus:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-2 my-10"
+       target="_blank"
+       href="https://github.com/FRFlor/social-mob/issues">
+        Have suggestions? Please, share them here!
+    </a>
 @endsection
