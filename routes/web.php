fb3a6a18f9308bc07fcaa158ac843c0dc0fb68c8
Felipe Flor <f.flor@vehikl.com>
2020-05-25 09:23:36 -0400

    Remove HomeController in favor of View Route

--- routes/web.php
+++ routes/web.php
@@ -19,7 +19,7 @@ Route::view('about','about')->name('about');
 Route::get('login/github', 'Auth\LoginController@redirectToProvider')->name('oauth.login.redirect');
 Route::get('oauth/callback', 'Auth\LoginController@handleProviderCallback')->name('oauth.login.callback');
 
-Route::get('/', 'HomeController@index')->name('home');
+Route::view('/', 'home')->name('home');
 Route::get('social_mob/week', 'SocialMobController@week')->name('social_mob.week');
 Route::post('social_mob/{social_mob}/join', 'SocialMobController@join')->name('social_mob.join');
 Route::post('social_mob/{social_mob}/leave', 'SocialMobController@leave')->name('social_mob.leave');
