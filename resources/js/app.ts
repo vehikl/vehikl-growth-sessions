094a72289900f1ea7adf3d170fdb4840b359f48a
adenysenko <a.denysenko@vehikl.com>
2020-06-05 18:09:44 -0400

    Add separate edit view

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,12 +2,13 @@ import "./bootstrap";
 import Vue from 'vue';
 import WeekView from "./components/WeekView.vue";
 import MobView from "./components/MobView.vue";
+import MobEdit from "./components/MobEdit.vue";
 import VModal from 'vue-js-modal';
 import moment from 'moment';
 Vue.use(VModal);
 new Vue({
     el: '#app',
-    components: {WeekView, MobView}
+    components: {WeekView, MobView, MobEdit}
 });
 
 
--- resources/views/social-mob-edit.blade.php
+++ resources/views/social-mob-edit.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.app')
 
 @section('content')
-    <mob-view class="mx-auto my-10" :mob-json="{{json_encode($socialMob)}}" :user="{{json_encode(auth()->user())}}"></mob-view>
+    <mob-edit class="mx-auto my-10" :mob-json="{{json_encode($socialMob)}}" :user="{{json_encode(auth()->user())}}"></mob-edit>
 @endsection
