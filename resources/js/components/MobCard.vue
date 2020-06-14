d9ffae19544b424c8138fb11c199096e0eb85060
Felipe Flor <f.flor@vehikl.com>
2020-06-14 01:46:37 -0400

    Move the grab handle closer to the card header

--- resources/js/components/MobCard.vue
+++ resources/js/components/MobCard.vue
@@ -1,61 +1,61 @@
 <template>
-    <div class="bg-gray-100 border border-blue-300 shadow rounded-lg hover:bg-blue-100 cursor-pointer"
+    <div class="bg-gray-100 border border-blue-300 p-6 shadow rounded-lg hover:bg-blue-100 cursor-pointer"
          @click="goToMob">
-        <div v-if="isDraggable"
-             @click.stop
-             class="p-3 z-10 handle cursor-grab w-full flex justify-end">
-            <icon-draggable class="h-6 text-blue-600 hover:text-blue-800"/>
-        </div>
-        <div class="p-6" :class="{'pt-0' : isDraggable}">
-            <div class="flex items-center">
+        <div class="flex">
+            <div class="flex items-center flex-1">
                 <v-avatar :src="socialMob.owner.avatar" :alt="`${socialMob.owner.name}'s Avatar`" size="12"/>
                 <h3 class="ml-6 text-lg" v-text="socialMob.owner.name"/>
             </div>
-            <pre
-                class="my-4 inline-block text-left whitespace-pre-wrap max-h-64 overflow-y-auto overflow-x-hidden font-sans"
-                v-text="socialMob.topic"/>
-            <div class="flex justify-between mb-2 text-blue-700">
-                <div class="flex items-center attendees-count">
-                    <i class="fa fa-user-circle text-lg mr-2" aria-hidden="true"></i>
-                    <span v-text="socialMob.attendees.length"/>
-                </div>
-                <div class="flex items-center">
-                    <i class="fa fa-clock-o text-lg mr-2" aria-hidden="true"></i>
-                    {{socialMob.startTime}} to {{socialMob.endTime}}
-                </div>
+            <div v-if="isDraggable"
+                 @click.stop
+                 class="z-10 handle cursor-grab">
+                <icon-draggable class="h-6 text-blue-600 hover:text-blue-800"/>
             </div>
-
-            <div class="text-blue-700 text-left mb-4 break-all">
-                <i class="fa fa-compass text-xl mr-1" aria-hidden="true"></i>
-                <a @click.stop v-if="socialMob.isLocationAnUrl" class="location underline" :href="socialMob.location"
-                   target="_blank" v-text="socialMob.location"/>
-                <span v-else class="location" v-text="socialMob.location"/>
+        </div>
+        <pre
+            class="my-4 inline-block text-left whitespace-pre-wrap max-h-64 overflow-y-auto overflow-x-hidden font-sans"
+            v-text="socialMob.topic"/>
+        <div class="flex justify-between mb-2 text-blue-700">
+            <div class="flex items-center attendees-count">
+                <i class="fa fa-user-circle text-lg mr-2" aria-hidden="true"></i>
+                <span v-text="socialMob.attendees.length"/>
             </div>
+            <div class="flex items-center">
+                <i class="fa fa-clock-o text-lg mr-2" aria-hidden="true"></i>
+                {{socialMob.startTime}} to {{socialMob.endTime}}
+            </div>
+        </div>
 
+        <div class="text-blue-700 text-left mb-4 break-all">
+            <i class="fa fa-compass text-xl mr-1" aria-hidden="true"></i>
+            <a @click.stop v-if="socialMob.isLocationAnUrl" class="location underline" :href="socialMob.location"
+               target="_blank" v-text="socialMob.location"/>
+            <span v-else class="location" v-text="socialMob.location"/>
+        </div>
+
+        <button
+            class="join-button w-32 bg-blue-500 hover:bg-blue-700 focus:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+            @click.stop="joinMob"
+            v-show="socialMob.canJoin(user)">
+            Join
+        </button>
+        <button
+            class="leave-button w-32 bg-red-500 hover:bg-red-700 focus:bg-red-700  text-white font-bold py-2 px-4 rounded"
+            @click.stop="leaveMob"
+            v-show="socialMob.canLeave(user)">
+            Leave
+        </button>
+        <div v-show="socialMob.canEditOrDelete(user)">
             <button
-                class="join-button w-32 bg-blue-500 hover:bg-blue-700 focus:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                @click.stop="joinMob"
-                v-show="socialMob.canJoin(user)">
-                Join
+                class="update-button w-32 bg-orange-500 hover:bg-orange-700 focus:bg-orange-700 text-white font-bold py-2 px-4 rounded"
+                @click.stop="$emit('edit-requested', socialMob)">
+                Edit
             </button>
             <button
-                class="leave-button w-32 bg-red-500 hover:bg-red-700 focus:bg-red-700  text-white font-bold py-2 px-4 rounded"
-                @click.stop="leaveMob"
-                v-show="socialMob.canLeave(user)">
-                Leave
+                class="delete-button w-16 bg-red-500 hover:bg-red-700 focus:bg-red-700 text-white font-bold py-2 px-4 rounded"
+                @click.stop="onDeleteClicked">
+                <i class="fa fa-trash" aria-hidden="true"></i>
             </button>
-            <div v-show="socialMob.canEditOrDelete(user)">
-                <button
-                    class="update-button w-32 bg-orange-500 hover:bg-orange-700 focus:bg-orange-700 text-white font-bold py-2 px-4 rounded"
-                    @click.stop="$emit('edit-requested', socialMob)">
-                    Edit
-                </button>
-                <button
-                    class="delete-button w-16 bg-red-500 hover:bg-red-700 focus:bg-red-700 text-white font-bold py-2 px-4 rounded"
-                    @click.stop="onDeleteClicked">
-                    <i class="fa fa-trash" aria-hidden="true"></i>
-                </button>
-            </div>
         </div>
     </div>
 </template>
@@ -100,6 +100,10 @@
 </script>
 
 <style lang="scss" scoped>
+    .handle {
+        margin: -0.7rem;
+    }
+
     .cursor-grab {
         cursor: grab;
 
