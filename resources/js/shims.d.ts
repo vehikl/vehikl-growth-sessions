fe1fa83738881801abc9cfb788569f92d5bc6899
Felipe Flor <f.flor@vehikl.com>
2020-06-15 00:06:30 -0400

    Account for json modules not being resolved in production

---
+++ resources/js/shims.d.ts
@@ -0,0 +1,13 @@
+declare module '*.vue' {
+    import Vue from 'vue';
+    export default Vue;
+}
+
+declare module '*.json' {
+    const value: any;
+    export default value;
+}
+
+declare module 'vue2-timepicker';
+declare module 'vuejs-datepicker';
+declare module 'vuedraggable';
--- resources/js/vue-shim.d.ts
+++
@@ -1,8 +0,0 @@
-declare module "*.vue" {
-    import Vue from 'vue';
-    export default Vue;
-}
-
-declare module "vue2-timepicker";
-declare module "vuejs-datepicker";
-declare module "vuedraggable";
