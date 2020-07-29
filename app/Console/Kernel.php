c9d70acd86e6d91cb79bccec54b06688df0c397d
Felipe Flor <f.flor@vehikl.com>
2020-07-29 10:01:33 -0400

    Disable automob

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,7 +2,6 @@
 
 namespace App\Console;
 
-use App\Console\Commands\AutoMobCommand;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -25,7 +24,6 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        $schedule->command(AutoMobCommand::class)->weekly();
     }
 
     /**
