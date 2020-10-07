5987c9e8143aaeb010e09ce582bbbefea85d4d5d
adenysenko <a.denysenko@vehikl.com>
2020-10-07 19:21:32 -0400

    Refactor tests

--- resources/js/components/MobView.spec.ts
+++ resources/js/components/MobView.spec.ts
@@ -1,46 +1,20 @@
 import {mount, Wrapper} from "@vue/test-utils";
 import MobView from "./MobView.vue";
 import userJson from '../../../tests/fixtures/User.json';
+import mobJson from '../../../tests/fixtures/SocialMobWithComments.json'
 import {User} from "../classes/User";
-
-const testMob = {
-    id: 123,
-    topic: 'test',
-    location: 'test',
-    date: '01/01/2020',
-    start_time: new Date().toDateString(),
-    end_time: new Date().toDateString(),
-    owner: userJson,
-    attendees: [
-        {
-            avatar: 'lastAirBender.jpg',
-            email: 'jack@bauer.com',
-            id: 987,
-            name: 'Cat',
-            username: 'cat'
-        },
-        {
-            avatar: 'lastAirBender.jpg',
-            email: 'jack@bauer.com',
-            id: 986,
-            name: 'Dog',
-            username: 'dog'
-        }
-    ],
-    comments: [],
-}
 
 describe('MobView', () => {
     let wrapper: Wrapper<MobView>;
 
     beforeEach(() => {
-        wrapper = mount(MobView, {propsData: {user: userJson, mobJson: testMob}});
+        wrapper = mount(MobView, {propsData: {user: userJson, mobJson}});
     });
 
     it('redirects to the owners GitHub page when clicked on the profile', async () => {
         const ownerComponent = wrapper.findComponent({ref: 'owner-avatar-link'})
 
-        expect(ownerComponent.element).toHaveAttribute('href', new User(testMob.owner).githubURL)
+        expect(ownerComponent.element).toHaveAttribute('href', new User(mobJson.owner).githubURL)
     })
 
     describe('attendees section', () => {
@@ -48,7 +22,7 @@ describe('MobView', () => {
             const attendeeComponents = wrapper.findAllComponents({ref: 'attendee'})
 
             attendeeComponents.wrappers.forEach((attendeeComponent, i) =>
-                expect(attendeeComponent.element).toHaveAttribute('href', new User(testMob.attendees[i]).githubURL)
+                expect(attendeeComponent.element).toHaveAttribute('href', new User(mobJson.attendees[i]).githubURL)
             )
         })
     })
