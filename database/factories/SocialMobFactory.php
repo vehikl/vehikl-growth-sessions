4a514d35c091dfb8841f2adca6282a32db160a70
Daniel Mills <daniel.mills@ableto.com>
2020-10-16 16:15:29 -0400

    Fix bug that caused the SocialMobFactory to create invalid social mobs

--- database/factories/SocialMobFactory.php
+++ database/factories/SocialMobFactory.php
@@ -6,14 +6,13 @@ use App\Model;
 use App\SocialMob;
 use App\User;
 use Faker\Generator as Faker;
-use Illuminate\Support\Str;
 
 $factory->define(SocialMob::class, function (Faker $faker) {
     return [
         'owner_id' => function() {
             return factory(User::class)->create()->id;
         },
-        'title' => Str::limit($faker->sentence, 45),
+        'title' => $faker->sentence(2),
         'topic' => $faker->sentence,
         'location' => 'At AnyDesk XYZ - abcdefg',
         'date' => today(),
