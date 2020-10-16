2621e51334b19196e40f2ebeb4c55cab71f33540
Daniel Mills <daniel.mills@ableto.com>
2020-10-16 16:11:24 -0400

    Fixed some tests

--- app/Http/Resources/SocialMobWeek.php
+++ app/Http/Resources/SocialMobWeek.php
@@ -10,28 +10,24 @@ use Illuminate\Support\Carbon;
 class SocialMobWeek extends ResourceCollection
 {
     public $collects = SocialMob::class;
-
     /**
      * Transform the resource into an array.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param \Illuminate\Http\Request $request
      * @return array
      */
     public function toArray($request)
     {
-        return $this->emptyWeek()->merge(collect(parent::toArray($request))->groupBy('date'));
+        $requestedDate = $request->input('date', now()->toDateString());
+        return $this->emptyWeek($requestedDate)->merge(collect(parent::toArray($request))->groupBy('date'))->toArray();
     }
 
-    private function emptyWeek()
+    private function emptyWeek($requestedDate)
     {
+        /** @var Carbon $monday */
+        $monday = ($this->collection->first()->date ?? Carbon::parse($requestedDate))->startOfWeek();
 
-        /** @var Carbon $firstSessionDate */
-        $firstSessionDate = $this->collection->first()->date;
-
-        $monday = $firstSessionDate->clone()->subDays(Carbon::MONDAY - $firstSessionDate->dayOfWeek);
-
-        return collect(CarbonPeriod::between($monday, $monday->clone()->addDays(4)))
-            ->mapWithKeys(fn ($date) => [$date->toDateString() => collect()]);
-
+        return collect(CarbonPeriod::between($monday, $monday->clone()->endOfWeek(Carbon::FRIDAY)))
+            ->mapWithKeys(fn($date) => [$date->toDateString() => collect()]);
     }
 }
