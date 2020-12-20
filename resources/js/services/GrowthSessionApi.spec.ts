779fd874a22131c4b480a79598e3d0c153c5a242
Vehikl Team <go@vehikl.com>
2020-12-20 14:33:26 -0500

    extract variable

--- resources/js/services/GrowthSessionApi.spec.ts
+++ resources/js/services/GrowthSessionApi.spec.ts
@@ -2,15 +2,13 @@ import axios from 'axios';
 import MockAdapter from 'axios-mock-adapter';
 import growthSessionsThisWeekJson from '../../../tests/fixtures/WeekGrowthSessions.json';
 import growthSessionWithComments from '../../../tests/fixtures/GrowthSessionWithComments.json';
-import {GrowthSessionApi} from './GrowthSessionApi';
+import {GrowthSessionApi, growthSessionResource} from './GrowthSessionApi';
 import {IGrowthSession, IUpdateGrowthSessionRequest} from '../types';
 import {DateTime} from '../classes/DateTime';
 import {WeekGrowthSessions} from '../classes/WeekGrowthSessions';
 
 const growthSessionsThisWeek: WeekGrowthSessions = new WeekGrowthSessions(growthSessionsThisWeekJson);
 const dummyGrowthSession: IGrowthSession = growthSessionWithComments;
-
-const resource = 'social_mobs';
 
 describe('GrowthSessionApi', () => {
     let mockBackend: MockAdapter;
@@ -25,7 +23,7 @@ describe('GrowthSessionApi', () => {
 
     it('returns the growth sessions of the current week if no date is provided', async () => {
         DateTime.setTestNow('2020-05-01');
-        mockBackend.onGet(`${resource}/week?date=2020-05-01`).reply(200, growthSessionsThisWeekJson);
+        mockBackend.onGet(`${growthSessionResource}/week?date=2020-05-01`).reply(200, growthSessionsThisWeekJson);
 
         const result = await GrowthSessionApi.getAllGrowthSessionsOfTheWeek();
 
@@ -35,7 +33,7 @@ describe('GrowthSessionApi', () => {
 
     it('returns the growth sessions of the week of the date provided', async() => {
         DateTime.setTestNow('2020-05-01');
-        mockBackend.onGet(`${resource}/week?date=2020-01-01`).reply(200, growthSessionsThisWeekJson);
+        mockBackend.onGet(`${growthSessionResource}/week?date=2020-01-01`).reply(200, growthSessionsThisWeekJson);
 
         const result = await GrowthSessionApi.getAllGrowthSessionsOfTheWeek('2020-01-01');
 
@@ -43,7 +41,7 @@ describe('GrowthSessionApi', () => {
     });
 
     it('stores a new growth session', async () => {
-        mockBackend.onPost(resource).reply(201, growthSessionWithComments);
+        mockBackend.onPost(growthSessionResource).reply(201, growthSessionWithComments);
 
         const result = await GrowthSessionApi.store(growthSessionWithComments);
 
@@ -52,7 +50,7 @@ describe('GrowthSessionApi', () => {
 
     it('updates an existing growth session', async () => {
         let newTopic: string = 'A totally new topic';
-        mockBackend.onPut(`${resource}/${dummyGrowthSession.id}`).reply(200, {
+        mockBackend.onPut(`${growthSessionResource}/${dummyGrowthSession.id}`).reply(200, {
             ...dummyGrowthSession,
             topic: newTopic
         });
@@ -64,7 +62,7 @@ describe('GrowthSessionApi', () => {
     });
 
     it('deletes an existing growth session', async () => {
-        mockBackend.onDelete(`${resource}/${dummyGrowthSession.id}`).reply(200);
+        mockBackend.onDelete(`${growthSessionResource}/${dummyGrowthSession.id}`).reply(200);
 
         const result = await GrowthSessionApi.delete(dummyGrowthSession);
 
@@ -72,36 +70,36 @@ describe('GrowthSessionApi', () => {
     });
 
     it('joins an existing growth session', async () => {
-        mockBackend.onPost(`${resource}/${dummyGrowthSession.id}/join`).reply(200, dummyGrowthSession);
+        mockBackend.onPost(`${growthSessionResource}/${dummyGrowthSession.id}/join`).reply(200, dummyGrowthSession);
 
         await GrowthSessionApi.join(dummyGrowthSession);
 
-        expect(mockBackend.history.post[0].url).toBe(`/${resource}/${dummyGrowthSession.id}/join`);
+        expect(mockBackend.history.post[0].url).toBe(`/${growthSessionResource}/${dummyGrowthSession.id}/join`);
     });
 
     it('leaves an existing growth session', async () => {
-        mockBackend.onPost(`${resource}/${dummyGrowthSession.id}/leave`).reply(200, dummyGrowthSession);
+        mockBackend.onPost(`${growthSessionResource}/${dummyGrowthSession.id}/leave`).reply(200, dummyGrowthSession);
 
         await GrowthSessionApi.leave(dummyGrowthSession);
 
-        expect(mockBackend.history.post[0].url).toBe(`/${resource}/${dummyGrowthSession.id}/leave`);
+        expect(mockBackend.history.post[0].url).toBe(`/${growthSessionResource}/${dummyGrowthSession.id}/leave`);
     });
 
     it('allows a new comment to be created', async () => {
-        mockBackend.onPost(`${resource}/${dummyGrowthSession.id}/comments`).reply(201, dummyGrowthSession);
+        mockBackend.onPost(`${growthSessionResource}/${dummyGrowthSession.id}/comments`).reply(201, dummyGrowthSession);
         const content = 'Hello world';
 
         await GrowthSessionApi.postComment(dummyGrowthSession, content);
 
-        expect(mockBackend.history.post[0].url).toBe(`/${resource}/${dummyGrowthSession.id}/comments`);
+        expect(mockBackend.history.post[0].url).toBe(`/${growthSessionResource}/${dummyGrowthSession.id}/comments`);
         expect(JSON.parse(mockBackend.history.post[0].data)).toEqual({content: content})
     });
 
     it('allows a comment to be deleted', async () => {
-        mockBackend.onDelete(`${resource}/${dummyGrowthSession.id}/comments/${dummyGrowthSession.comments[0].id}`).reply(200, dummyGrowthSession);
+        mockBackend.onDelete(`${growthSessionResource}/${dummyGrowthSession.id}/comments/${dummyGrowthSession.comments[0].id}`).reply(200, dummyGrowthSession);
 
         await GrowthSessionApi.deleteComment(dummyGrowthSession.comments[0]);
 
-        expect(mockBackend.history.delete[0].url).toBe(`/${resource}/${dummyGrowthSession.id}/comments/${dummyGrowthSession.comments[0].id}`);
+        expect(mockBackend.history.delete[0].url).toBe(`/${growthSessionResource}/${dummyGrowthSession.id}/comments/${dummyGrowthSession.comments[0].id}`);
     });
 });
--- resources/js/services/GrowthSessionApi.ts
+++ resources/js/services/GrowthSessionApi.ts
@@ -4,53 +4,55 @@ import {DateTime} from '../classes/DateTime';
 import {GrowthSession} from '../classes/GrowthSession';
 import {WeekGrowthSessions} from '../classes/WeekGrowthSessions';
 
+export const growthSessionResource = 'social_mobs';
+
 export class GrowthSessionApi extends BaseApi {
     static async getAllGrowthSessionsOfTheWeek(date: string = DateTime.today().toDateString()): Promise<WeekGrowthSessions> {
         let dateString = DateTime.parseByDate(date).toDateString();
-        let response = await BaseApi.httpRequest.get<IWeekGrowthSessions>(`/social_mobs/week?date=${dateString}`);
+        let response = await BaseApi.httpRequest.get<IWeekGrowthSessions>(`/${growthSessionResource}/week?date=${dateString}`);
         return new WeekGrowthSessions(response.data);
     }
 
     static async store(payload: IStoreGrowthSessionRequest): Promise<GrowthSession> {
-        let response = await BaseApi.httpRequest.post<IGrowthSession>('/social_mobs', payload);
+        let response = await BaseApi.httpRequest.post<IGrowthSession>(`/${growthSessionResource}`, payload);
         return new GrowthSession(response.data);
     }
 
     static async update(target: IGrowthSession, payload: IUpdateGrowthSessionRequest): Promise<GrowthSession> {
-        let response = await BaseApi.httpRequest.put<IGrowthSession>(`/social_mobs/${target.id}`, payload);
+        let response = await BaseApi.httpRequest.put<IGrowthSession>(`/${growthSessionResource}/${target.id}`, payload);
         return new GrowthSession(response.data);
     }
 
     static async delete(target: IGrowthSession): Promise<boolean> {
-        await BaseApi.httpRequest.delete(`/social_mobs/${target.id}`);
+        await BaseApi.httpRequest.delete(`/${growthSessionResource}/${target.id}`);
         return true;
     }
 
     static async join(target: IGrowthSession): Promise<GrowthSession> {
-        let response = await BaseApi.httpRequest.post(`/social_mobs/${target.id}/join`);
+        let response = await BaseApi.httpRequest.post(`/${growthSessionResource}/${target.id}/join`);
         return new GrowthSession(response.data);
     }
 
     static async leave(target: IGrowthSession): Promise<GrowthSession> {
-        let response = await BaseApi.httpRequest.post(`/social_mobs/${target.id}/leave`);
+        let response = await BaseApi.httpRequest.post(`/${growthSessionResource}/${target.id}/leave`);
         return new GrowthSession(response.data);
     }
 
     static async postComment(target: IGrowthSession, content: string): Promise<GrowthSession> {
-        let response = await BaseApi.httpRequest.post(`/social_mobs/${target.id}/comments`, {content});
+        let response = await BaseApi.httpRequest.post(`/${growthSessionResource}/${target.id}/comments`, {content});
         return new GrowthSession(response.data);
     }
 
     static async deleteComment(comment: IComment): Promise<GrowthSession> {
-        let response = await BaseApi.httpRequest.delete(`/social_mobs/${comment.social_mob_id}/comments/${comment.id}`);
+        let response = await BaseApi.httpRequest.delete(`/${growthSessionResource}/${comment.social_mob_id}/comments/${comment.id}`);
         return new GrowthSession(response.data);
     }
 
     static editUrl(target: IGrowthSession): string {
-        return `/social_mobs/${target.id}/edit`
+        return `/${growthSessionResource}/${target.id}/edit`
     }
 
     static showUrl(target: IGrowthSession): string {
-        return `/social_mobs/${target.id}`;
+        return `/${growthSessionResource}/${target.id}`;
     }
 }
