a8481b18552fb1cfeb239c4db830a3d665a1543b
Vehikl Team <go@vehikl.com>
2020-12-20 13:43:29 -0500

    remove clutter

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -2,7 +2,6 @@ import "./bootstrap";
 import Vue from 'vue';
 import WeekView from "./components/WeekView.vue";
 import GrowthSessionView from "./components/GrowthSessionView.vue";
-import GrowthSessionEdit from "./components/GrowthSessionEdit.vue";
 import VAvatar from "./components/VAvatar.vue";
 import VModal from 'vue-js-modal';
 
@@ -10,5 +9,5 @@ Vue.use(VModal);
 
 new Vue({
     el: '#app',
-    components: {WeekView, GrowthSessionView, GrowthSessionEdit, VAvatar}
+    components: {WeekView, GrowthSessionView, VAvatar}
 });
