8975ebc9088a2cb54bc25e1e4147e070dbb6ec45
Vehikl Team <go@vehikl.com>
2020-12-20 13:30:59 -0500

    rename variable

--- resources/js/components/GrowthSessionView.vue
+++ resources/js/components/GrowthSessionView.vue
@@ -2,34 +2,34 @@
     <div class="max-w-5xl text-blue-600">
         <div class="mb-8 flex flex-col lg:flex-row lg:justify-between items-center">
             <h2 class="text-2xl lg:text-3xl font-sans font-light flex items-center text-blue-700">
-                <a :href="mob.owner.githubURL" ref="owner-avatar-link">
-                    <v-avatar class="mr-4" :src="mob.owner.avatar" :alt="`${mob.owner.name}'s Avatar`"/>
+                <a :href="growthSession.owner.githubURL" ref="owner-avatar-link">
+                    <v-avatar class="mr-4" :src="growthSession.owner.avatar" :alt="`${growthSession.owner.name}'s Avatar`"/>
                 </a>
-                {{mob.title}}
+                {{ growthSession.title }}
             </h2>
             <div>
                 <button
                     class="join-button w-32 bg-blue-500 hover:bg-blue-700 focus:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                    @click.stop="mob.join()"
-                    v-show="mob.canJoin(userJson)">
+                    @click.stop="growthSession.join()"
+                    v-show="growthSession.canJoin(userJson)">
                     Join
                 </button>
                 <button
                     class="leave-button w-32 bg-red-500 hover:bg-red-700 focus:bg-red-700  text-white font-bold py-2 px-4 rounded"
-                    @click.stop="mob.leave()"
-                    v-show="mob.canLeave(userJson)">
+                    @click.stop="growthSession.leave()"
+                    v-show="growthSession.canLeave(userJson)">
                     Leave
                 </button>
                 <button
                     class="update-button w-32 bg-orange-500 hover:bg-orange-700 focus:bg-orange-700 text-white font-bold py-2 px-4 rounded"
                     @click.stop="editMob"
-                    v-if="mob.canEditOrDelete(userJson)">
+                    v-if="growthSession.canEditOrDelete(userJson)">
                     Edit
                 </button>
                 <button
                     class="delete-button w-16 bg-red-500 hover:bg-red-700 focus:bg-red-700 text-white font-bold py-2 px-4 rounded"
                     @click.stop="deleteMob"
-                    v-if="mob.canEditOrDelete(userJson)">
+                    v-if="growthSession.canEditOrDelete(userJson)">
                     <i class="fa fa-trash" aria-hidden="true"></i>
                 </button>
             </div>
@@ -37,12 +37,12 @@
         <div class="flex flex-col lg:flex-row flex-wrap">
             <div class="flex-1 mr-2 max-w-5xl">
                 <h3 class="text-2xl font-sans font-light mb-3 text-blue-700">Topic</h3>
-                <pre class="font-sans m-5 break-words-fixed whitespace-pre-wrap" v-text="mob.topic"/>
+                <pre class="font-sans m-5 break-words-fixed whitespace-pre-wrap" v-text="growthSession.topic"/>
             </div>
             <div class="flex-none max-w-md">
                 <div class="mb-3">
                     <h3 class="text-2xl font-sans inline font-light mr-3 text-blue-700">Location:</h3>
-                    <location-renderer :location-string="mob.location"/>
+                    <location-renderer :location-string="growthSession.location"/>
                 </div>
 
                 <div class="mb-3">
@@ -51,14 +51,14 @@
                     <span class="whitespace-no-wrap">( {{time}} )</span>
                 </div>
 
-                <div class="mb-3" v-if="mob.attendee_limit">
+                <div class="mb-3" v-if="growthSession.attendee_limit">
                     <h3 class="text-2xl font-sans inline font-light mr-3 text-blue-700">Attendee Limit:</h3>
-                    <span class="attendee_limit">{{ mob.attendee_limit }}</span>
+                    <span class="attendee_limit">{{ growthSession.attendee_limit }}</span>
                 </div>
 
                 <h3 class="text-2xl font-sans font-light mb-3 text-blue-700">Attendees</h3>
                 <ul>
-                    <li v-for="attendee in mob.attendees">
+                    <li v-for="attendee in growthSession.attendees">
                         <a ref="attendee" :href="attendee.githubURL" class="flex items-center ml-6 my-4">
                             <v-avatar size="12" class="mr-3" :src="attendee.avatar" :alt="`${attendee.name}'s Avatar`"/>
                             {{attendee.name}}
@@ -67,7 +67,7 @@
                 </ul>
             </div>
         </div>
-        <comment-list class="mt-24 max-w-xl" :growth-session="mob" :user="userJson"/>
+        <comment-list class="mt-24 max-w-xl" :growth-session="growthSession" :user="userJson"/>
     </div>
 </template>
 
@@ -87,23 +87,23 @@ import LocationRenderer from './LocationRenderer.vue';
     export default class MobView extends Vue {
         @Prop({required: false}) userJson!: IUser;
         @Prop({required: true}) growthSessionJson!: IGrowthSession;
-        mob: GrowthSession = new GrowthSession(this.growthSessionJson);
+        growthSession: GrowthSession = new GrowthSession(this.growthSessionJson);
 
         get date(): string {
-            return `${DateTime.parseByDate(this.mob.date).format('MMM-DD')}`
+            return `${DateTime.parseByDate(this.growthSession.date).format('MMM-DD')}`
         }
 
         get time(): string {
-            return `${this.mob.startTime} - ${this.mob.endTime}`;
+            return `${this.growthSession.startTime} - ${this.growthSession.endTime}`;
         }
 
         async deleteMob() {
-            await this.mob.delete();
+            await this.growthSession.delete();
             window.location.assign('/');
         }
 
         async editMob() {
-            window.location.assign(GrowthSessionApi.editUrl(this.mob));
+            window.location.assign(GrowthSessionApi.editUrl(this.growthSession));
         }
     }
 </script>
