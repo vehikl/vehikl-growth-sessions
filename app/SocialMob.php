9db503bfc75065bd023c45ba3e9bb27c34075989
vehikl <vehikl@vehikl.com>
2020-12-19 19:01:36 -0500

    remove clutter

--- app/SocialMob.php
+++ app/SocialMob.php
@@ -4,7 +4,6 @@ namespace App;
 
 use Carbon\Carbon;
 use Carbon\CarbonImmutable;
-use Carbon\CarbonPeriod;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
