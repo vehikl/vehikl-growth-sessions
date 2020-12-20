7d3ebdd7b99fd062222e619eca8e38d7bd5b0e1d
Vehikl Team <go@vehikl.com>
2020-12-20 12:42:43 -0500

    rename variables

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,8 +1,8 @@
 import "./bootstrap";
 import Vue from 'vue';
 import WeekView from "./components/WeekView.vue";
-import MobView from "./components/GrowthSessionView.vue";
-import MobEdit from "./components/GrowthSessionEdit.vue";
+import GrowthSessionView from "./components/GrowthSessionView.vue";
+import GrowthSessionEdit from "./components/GrowthSessionEdit.vue";
 import VAvatar from "./components/VAvatar.vue";
 import VModal from 'vue-js-modal';
 
@@ -10,5 +10,5 @@ Vue.use(VModal);
 
 new Vue({
     el: '#app',
-    components: {WeekView, MobView, MobEdit, VAvatar}
+    components: {WeekView, GrowthSessionView, GrowthSessionEdit, VAvatar}
 });
