e949d7c93741c19c84dbf35c52de750da8b99f05
Vehikl Team <go@vehikl.com>
2020-12-20 12:41:00 -0500

    Update imports

--- resources/js/app.ts
+++ resources/js/app.ts
@@ -1,8 +1,8 @@
 import "./bootstrap";
 import Vue from 'vue';
 import WeekView from "./components/WeekView.vue";
-import MobView from "./components/MobView.vue";
-import MobEdit from "./components/MobEdit.vue";
+import MobView from "./components/GrowthSessionView.vue";
+import MobEdit from "./components/GrowthSessionEdit.vue";
 import VAvatar from "./components/VAvatar.vue";
 import VModal from 'vue-js-modal';
 
