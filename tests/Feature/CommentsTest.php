6ccd64c6a36456cd400c61ea720d15ba1b1b1e32
vehikl <vehikl@vehikl.com>
2020-12-19 19:14:34 -0500

    rename

--- tests/Feature/CommentsTest.php
+++ tests/Feature/CommentsTest.php
@@ -3,7 +3,7 @@
 namespace Tests\Feature;
 
 use App\Comment;
-use App\SocialMob;
+use App\SocialMob as GrowthSession;
 use App\User;
 use Illuminate\Http\Response;
 use Tests\TestCase;
@@ -13,7 +13,7 @@ class CommentsTest extends TestCase
     public function testAUserCanPostCommentsOnAnExistingGrowthSession()
     {
         $user = User::factory()->create();
-        $growthSession = SocialMob::factory()->create();
+        $growthSession = GrowthSession::factory()->create();
 
         $this->actingAs($user)
             ->postJson(route('social_mobs.comments.store', $growthSession), ['content' => 'Hello world'])
@@ -24,7 +24,7 @@ class CommentsTest extends TestCase
 
     public function testItDoesNotAllowGuestsToPostComments()
     {
-        $socialMob = SocialMob::factory()->create();
+        $socialMob = GrowthSession::factory()->create();
 
         $this->postJson(route('social_mobs.comments.store', $socialMob), ['content' => 'Hello world'])
             ->assertStatus(Response::HTTP_UNAUTHORIZED);
@@ -32,7 +32,7 @@ class CommentsTest extends TestCase
 
     public function testAGuestCanGetAllCommentsOfAMob()
     {
-        $socialMob = SocialMob::factory()->create();
+        $socialMob = GrowthSession::factory()->create();
         $comments = Comment::factory()->times(4)->create(['social_mob_id' => $socialMob->id]);
 
         $this->getJson(route('social_mobs.comments.index', $socialMob))->assertJson($comments->toArray());
