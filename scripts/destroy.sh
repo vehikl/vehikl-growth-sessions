50138ac04379658ba1cd9b03d9e69197f641a055
Grant Lovell <g.lovell@vehikl.com>
2021-01-14 20:54:05 -0500

    Run commands from the project root
    No matter where the script is called from

--- scripts/destroy.sh
+++ scripts/destroy.sh
@@ -1,6 +1,14 @@
 #!/bin/sh
 
-cd ../
+realpath() {
+    [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
+}
+
+FILE_PATH=$(realpath $0)
+DIR_PATH=$(dirname $FILE_PATH)
+PROJECT_PATH=$(dirname $DIR_PATH)
+
+cd $PROJECT_PATH
 
 rm -rf node_modules
 rm -rf var
