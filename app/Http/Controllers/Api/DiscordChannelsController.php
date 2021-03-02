19be357b1ed0f48ddad40dbf849348452e422d28
Christian Griffin <c.griffin@vehikl.com>
2021-03-02 13:38:25 -0500

    add discord channels endpoint

---
+++ app/Http/Controllers/Api/DiscordChannelsController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Events\GrowthSessionAttendeeChanged;
+use App\Events\GrowthSessionCreated;
+use App\Events\GrowthSessionDeleted;
+use App\Events\GrowthSessionUpdated;
+use App\Exceptions\AttendeeLimitReached;
+use App\Http\Controllers\Controller;
+use App\Http\Requests\DeleteGrowthSessionRequest;
+use App\Http\Requests\StoreGrowthSessionRequest;
+use App\Http\Requests\UpdateGrowthSessionRequest;
+use App\Http\Resources\GrowthSession as GrowthSessionResource;
+use App\Http\Resources\GrowthSessionWeek;
+use App\GrowthSession;
+use App\Services\Discord\DiscordService;
+use App\Services\Discord\Models\Channel;
+use Illuminate\Http\Request;
+
+class DiscordChannelsController extends Controller
+{
+    public function index(DiscordService $discord)
+    {
+        return [
+            'data' => $discord->getChannels()
+                ->map(fn(Channel $channel) => ['id' => $channel->id, 'name' => $channel->name])
+                ->toArray()
+        ];
+    }
+}
