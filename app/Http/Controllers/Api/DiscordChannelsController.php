62c986d0072cf3fa37e21419e898d1b1bcff8c01
Christian Griffin <c.griffin@vehikl.com>
2021-03-03 09:11:58 -0500

    remove unneeded imports

--- app/Http/Controllers/Api/DiscordChannelsController.php
+++ app/Http/Controllers/Api/DiscordChannelsController.php
@@ -2,22 +2,9 @@
 
 namespace App\Http\Controllers\Api;
 
-use App\Events\GrowthSessionAttendeeChanged;
-use App\Events\GrowthSessionCreated;
-use App\Events\GrowthSessionDeleted;
-use App\Events\GrowthSessionUpdated;
-use App\Exceptions\AttendeeLimitReached;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\DeleteGrowthSessionRequest;
-use App\Http\Requests\StoreGrowthSessionRequest;
-use App\Http\Requests\UpdateGrowthSessionRequest;
 use App\Http\Resources\DiscordChannelCollection;
-use App\Http\Resources\GrowthSession as GrowthSessionResource;
-use App\Http\Resources\GrowthSessionWeek;
-use App\GrowthSession;
 use App\Services\Discord\DiscordService;
-use App\Services\Discord\Models\Channel;
-use Illuminate\Http\Request;
 
 class DiscordChannelsController extends Controller
 {
