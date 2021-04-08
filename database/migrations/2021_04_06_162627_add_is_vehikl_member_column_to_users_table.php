165d05ee75bcf5ae572e52be33efd27c68660cb7
Vehikl Team <go@vehikl.com>
2021-04-08 11:13:18 -0400

    Allow vehikl member to be nullable to facilitate migration in production

--- database/migrations/2021_04_06_162627_add_is_vehikl_member_column_to_users_table.php
+++ database/migrations/2021_04_06_162627_add_is_vehikl_member_column_to_users_table.php
@@ -14,7 +14,7 @@ class AddIsVehiklMemberColumnToUsersTable extends Migration
     public function up()
     {
         Schema::table('users', function (Blueprint $table) {
-            $table->boolean('is_vehikl_member')->default(false);
+            $table->boolean('is_vehikl_member')->nullable()->default(false);
         });
     }
 
