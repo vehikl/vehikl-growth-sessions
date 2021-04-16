ca62a1c6cefe1dcc234ead32035f3c9f3c79e598
Vehikl Team <go@vehikl.com>
2021-04-16 16:54:14 -0400

    Fix seeder

--- database/seeders/GrowthSessionSeeder.php
+++ database/seeders/GrowthSessionSeeder.php
@@ -3,6 +3,7 @@
 namespace Database\Seeders;
 
 use App\GrowthSession;
+use App\User;
 use Carbon\Carbon;
 use Carbon\CarbonImmutable;
 use Illuminate\Database\Seeder;
@@ -15,10 +16,12 @@ class GrowthSessionSeeder extends Seeder
         $monday = today()->isDayOfWeek($MONDAY) ? CarbonImmutable::today() : CarbonImmutable::parse('Last Monday');
         $numberOfFakeGrowthSessions = 5;
         for ($i = 0; $i < $numberOfFakeGrowthSessions; $i++) {
-            GrowthSession::factory()->create([
+            $growthSession = GrowthSession::factory()->create([
                 'date' => $monday->addDays(random_int(0, 4)),
                 'start_time' => Carbon::createFromTime(random_int(15, 16))
             ]);
+            $owner = User::factory()->vehiklMember()->create();
+            $owner->growthSessions()->attach($growthSession, ['user_type' => User::OWNER]);
         }
     }
 }
