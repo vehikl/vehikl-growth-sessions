66808cab26da236afb8104c4d1eed90220d386f8
Vehikl Team <go@vehikl.com>
2021-04-16 16:58:42 -0400

    sneaky

--- database/migrations/2021_04_16_135431_remove_owner_id_column_from_growth_sessions_table.php
+++ database/migrations/2021_04_16_135431_remove_owner_id_column_from_growth_sessions_table.php
@@ -9,7 +9,9 @@ class RemoveOwnerIdColumnFromGrowthSessionsTable extends Migration
     public function up()
     {
         Schema::table('growth_sessions', function (Blueprint $table) {
-            $table->dropForeign('social_mobs_owner_id_foreign');
+            if (config('database.default') !== "sqlite") {
+                $table->dropForeign('social_mobs_owner_id_foreign');
+            }
             $table->dropColumn('owner_id');
         });
     }
