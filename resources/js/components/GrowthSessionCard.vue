b31bf0bad71dbed72dbbc27e8fa597b0862b25a2
Vehikl Team <go@vehikl.com>
2021-07-26 16:58:52 -0400

    make draggable work again

--- resources/js/components/GrowthSessionCard.vue
+++ resources/js/components/GrowthSessionCard.vue
@@ -22,7 +22,7 @@
 
             <div v-if="isDraggable"
                  @click.stop
-                 class="z-10 cursor-grab h-10 w-10 hover:bg-blue-200 rounded-full inline-flex justify-center items-center">
+                 class="z-10 handle cursor-grab h-10 w-10 hover:bg-blue-200 rounded-full inline-flex justify-center items-center">
                 <icon-draggable class="h-4 text-blue-600 hover:text-blue-800"/>
             </div>
 
