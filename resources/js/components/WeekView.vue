e7227d0a2413a7e23b1cb99a475248a9372c45fc
Vehikl Team <go@vehikl.com>
2021-08-16 16:32:15 -0400

    Fix bug that caused day issues button to not be clickable
    Solves issue #123

--- resources/js/components/WeekView.vue
+++ resources/js/components/WeekView.vue
@@ -36,7 +36,7 @@
                  'bg-blue-200 border-blue-300': !date.isEvenDate(),
                  }"
                  :weekDay="date.weekDayString()"
-                 class="day text-center mx-1 mb-2 relative rounded border">
+                 class="day flex flex-col text-center mx-1 mb-2 relative rounded border">
                 <h3
                     class="text-lg text-blue-700 font-bold p-3 md:pt-6 sticky sm:relative top-0 w-full z-20 border-b md:border-b-0 rounded-t md:rounded-none mb-2 md:mb-0"
                     v-text="date.weekDayString()"
