1a6aad9a2c39433d84a79d100b1bdd77c71716bf
Vehikl Team <go@vehikl.com>
2021-08-24 17:02:28 -0400

    WIP - Making new card

--- resources/js/components/GrowthSessionCardNew.vue
+++ resources/js/components/GrowthSessionCardNew.vue
@@ -1,84 +1,21 @@
 <template>
-    <a class="block bg-gray-100 border py-4 px-6 shadow rounded-lg hover:bg-blue-100 cursor-pointer"
-       :class="growthSession.is_public ? 'border-blue-300' : 'border-vehikl-orange'"
-         :href="growthSessionUrl">
-        <div class="flex justify-end items-center" :class="{'mb-2': growthSession.title}" v-if="growthSession.title || isDraggable">
-            <a
-                :href="growthSession.calendarUrl"
-                target="_blank"
-                ref="add-to-calendar"
-                class="text-blue-700 leading-none hover:bg-blue-200 rounded-full h-10 w-10 inline-flex justify-center items-center"
-            >
-                <i aria-hidden="true" class="fa fa-calendar"></i>
-            </a>
-            <button
-                v-show="growthSession.canEditOrDelete(user)"
-                class="update-button text-blue-700 leading-none hover:bg-blue-200 rounded-full h-10 w-10 inline-flex justify-center items-center"
-                @click.prevent="$emit('edit-requested', growthSession)">
-                <i aria-hidden="true" class="fa fa-edit"></i>
-            </button>
-            <button
-                v-show="user && user.is_vehikl_member"
-                class="copy-button text-blue-700 leading-none hover:bg-blue-200 rounded-full h-10 w-10 inline-flex justify-center items-center"
-                @click.prevent="$emit('copy-requested', growthSession)">
-                <i aria-hidden="true" class="fa fa-copy"></i>
-            </button>
-            <button
-                v-show="growthSession.canEditOrDelete(user)"
-                class="delete-button text-blue-700 leading-none hover:bg-blue-200 rounded-full h-10 w-10 inline-flex justify-center items-center"
-                @click.prevent="onDeleteClicked">
-                <i class="fa fa-trash" aria-hidden="true"></i>
-            </button>
-
-            <div v-if="isDraggable"
-                 @click.stop
-                 class="z-10 handle h-10 w-10 hover:bg-blue-200 rounded-full inline-flex justify-center items-center cursor-move">
-                <icon-draggable class="h-4 text-blue-600 hover:text-blue-800"/>
+    <a class="bg-gray-100 max-w-sm block p-4 border-gray-300 border rounded-lg">
+        <div class="flex items-start">
+            <div class="bg-gray-600 text-white rounded h-16 w-16 flex flex-col items-start justify-center px-2 leading-none text-lg">
+                11:30
+                <div class="text-xs">PM</div>
             </div>
-
-        </div>
-        <h3 class="font-light text-lg text-blue-700 text-left mb-3"
-            :class="{'pr-4': isDraggable}"
-            ref="growth-session-title"
-            v-text="growthSession.title"/>
-        <pre
-            class="mb-4 inline-block text-left break-words-fixed whitespace-pre-wrap max-h-64 overflow-y-auto overflow-x-hidden font-sans"
-            ref="growth-session-topic"
-            v-text="growthSession.topic"/>
-        <div class="flex items-center flex-1 mb-4 text-blue-700">
-            <p class="mr-3">Host:</p>
-            <p class="mr-6 text-md" v-text="growthSession.owner.name"/>
-            <v-avatar :src="growthSession.owner.avatar" :alt="`${growthSession.owner.name}'s Avatar`" size="6"/>
-        </div>
-        <div class="flex justify-between mb-2 text-blue-700">
-            <div class="flex items-center attendees-count">
-                <i class="fa fa-user-circle text-lg mr-2" aria-hidden="true"></i>
-                <span v-text="growthSession.attendees.length"/>
-                <span v-if="growthSession.attendee_limit" class="pl-1 attendee-limit">of {{ growthSession.attendee_limit }}</span>
-            </div>
-            <div class="flex items-center">
-                <i class="fa fa-clock-o text-lg mr-2" aria-hidden="true"></i>
-                {{growthSession.startTime}} to {{growthSession.endTime}}
+            <div class="ml-2">
+                <h2 class="font-semibold text-lg mb-2">Finishing Off our multi-group drop down menu</h2>
+                <p class="text-gray-600 mb-2">Darren Galway</p>
+                <span class="bg-gray-300 rounded-full px-3 py-1 text-gray-600 text-sm">Private</span>
+                <span class="bg-gray-300 rounded-full px-3 py-1 text-gray-600 text-sm">1 hour</span>
+                <div class="flex items-center mt-5">
+                    <p class="text-gray-700">4 spots remaining</p>
+                    <button class="ml-auto text-blue-500 font-bold text">Join</button>
+                </div>
             </div>
         </div>
-
-        <div class="text-blue-700 text-left mb-4 break-all">
-            <i class="fa fa-compass text-xl mr-1" aria-hidden="true"></i>
-            <location-renderer :locationString="growthSession.location"/>
-        </div>
-
-        <button
-            class="join-button w-32 bg-blue-500 hover:bg-blue-700 focus:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-            @click.prevent="joinGrowthSession"
-            v-show="growthSession.canJoin(user)">
-            Join
-        </button>
-        <button
-            class="leave-button w-32 bg-red-500 hover:bg-red-700 focus:bg-red-700  text-white font-bold py-2 px-4 rounded"
-            @click.prevent="leaveGrowthSession"
-            v-show="growthSession.canLeave(user)">
-            Leave
-        </button>
     </a>
 </template>
 
