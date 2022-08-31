878d2d1faa5e7c180509c21cd2c0de61dad07f5c
Vehikl Team <go@vehikl.com>
2022-08-31 09:00:19 -0400

    use sudo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,13 +3,13 @@ echo "${timestamp}"
 
 pathToDeploy="/var/www/growth-sessions/${timestamp}"
 
-cp -r test ${pathToDeploy}
-chown -R www-data:www-data ${pathToDeploy}
+sudo cp -r test ${pathToDeploy}
+sudo chown -R www-data:www-data ${pathToDeploy}
 
-find ${pathToDeploy} -type f -exec chmod 644 {} \;
-find ${pathToDeploy} -type d -exec chmod 755 {} \;
+sudo find ${pathToDeploy} -type f -exec chmod 644 {} \;
+sudo find ${pathToDeploy} -type d -exec chmod 755 {} \;
 
 cd ${pathToDeploy}
 
-chgrp -R www-data storage bootstrap/cache
-chmod -R ug+rwx storage bootstrap/cache
+sudo chgrp -R www-data storage bootstrap/cache
+sudo chmod -R ug+rwx storage bootstrap/cache
