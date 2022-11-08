85570b9ed7ce47ffff1d19deef995e67b4222e59
Vehikl Team <go@vehikl.com>
2022-11-08 08:43:44 -0500

    return next week's growth sessions if it's a Saturday

--- app/GrowthSession.php
+++ app/GrowthSession.php
@@ -92,6 +92,10 @@ class GrowthSession extends Model
     {
         $referenceDate = CarbonImmutable::parse($referenceDate);
 
+        if ($referenceDate->isSaturday()) {
+            $referenceDate = $referenceDate->addDays(5);
+        }
+
         $startPoint = $referenceDate->isDayOfWeek(Carbon::MONDAY)
             ? $referenceDate
             : $referenceDate->modify('Last Monday');
