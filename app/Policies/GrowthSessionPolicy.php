ba3837a5ad5cb50eb2efa8346e8d72ba31d63fd4
Vehikl Team <go@vehikl.com>
2022-12-07 08:53:36 -0500

    Refactor

--- app/Policies/GrowthSessionPolicy.php
+++ app/Policies/GrowthSessionPolicy.php
@@ -62,7 +62,8 @@ class GrowthSessionPolicy
 
     public function watch(User $user, GrowthSession $growthSession)
     {
-        return !$growthSession->hasWatcher($user) && $this->isInTheFuture($growthSession);
+        return !$growthSession->hasWatcher($user) && !$growthSession->hasAttendee($user) && $this->isInTheFuture
+            ($growthSession);
     }
 
 
