1638ba8d12a03e7b433429ade110c94878401a35
Felipe Flor <f.flor@vehikl.com>
2024-06-26 09:41:16 -0400

    Fix type

--- resources/js/services/TagsApi.ts
+++ resources/js/services/TagsApi.ts
@@ -1,9 +1,9 @@
 import BaseApi from './BaseApi';
-import {IAnyDesk} from "../types";
+import {ITag} from "../types";
 
 export class TagsApi extends BaseApi {
-    static async index(): Promise<IAnyDesk[]> {
-        let response = await BaseApi.httpRequest.get<IAnyDesk[]>(`/tags`);
+    static async index(): Promise<ITag[]> {
+        let response = await BaseApi.httpRequest.get<ITag[]>(`/tags`);
         return response.data;
     }
 }
