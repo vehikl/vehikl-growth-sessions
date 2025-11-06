025f2386105685c57ea320713de650d660e81cc1
Felipe Flor <f.flor@vehikl.com>
2025-11-06 00:14:37 -0500

    Hide proposals link

--- resources/js/layouts/AppLayout.vue
+++ resources/js/layouts/AppLayout.vue
@@ -66,10 +66,13 @@ const isExpanded = ref(false);
                             </Link>
                             <Link v-else :href="route('statistics.index')" class="mt-4 hover:text-vehikl-orange transition-smooth sm:ml-6 lg:mt-0"> Statistics </Link>
                         </template>
-                        <Link v-if="route().current('proposals.index')" :href="route('home')" class="mt-4 hover:text-vehikl-orange transition-smooth sm:ml-6 lg:mt-0">
-                            Board
-                        </Link>
-                        <Link v-else :href="route('proposals.index')" class="mt-4 hover:text-vehikl-orange transition-smooth sm:ml-6 lg:mt-0">Proposals</Link>
+
+                        <template v-if="false">
+                            <Link v-if="route().current('proposals.index')" :href="route('home')" class="mt-4 hover:text-vehikl-orange transition-smooth sm:ml-6 lg:mt-0">
+                                Board
+                            </Link>
+                            <Link v-else :href="route('proposals.index')" class="mt-4 hover:text-vehikl-orange transition-smooth sm:ml-6 lg:mt-0">Proposals</Link>
+                        </template>
                     </template>
                     <Link v-if="route().current('about')" :href="route('home')" class="mt-4 hover:text-vehikl-orange transition-smooth sm:ml-6 lg:mt-0">
                         Board
