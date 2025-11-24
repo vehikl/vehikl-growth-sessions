bad22bf75cc4e4c669dba25894783b5a999de7b7
Vehikl Team <nomorobo@vehikl.com>
2025-11-24 15:41:39 -0500

    load bearing comment

--- resources/js/services/DiscordChannelApi.ts
+++ resources/js/services/DiscordChannelApi.ts
@@ -8,7 +8,10 @@ export class DiscordChannelApi extends BaseApi {
         const response = await BaseApi.httpRequest.get<Array<IDiscordChannel>>(`/${discordChannelResource}`);
         return response.data;
     }
-
+// HELLO FUTURE IAN
+    //THIS IS RETURNING CHANNELS THAT ARE NOT OCCUPIED
+    // MAL DID A 10-09 and a 10-10 and bot hwere marked occupied even though only the 9th was used
+    // TODO: Fix it I guess
     static async occupied(date): Promise<Array<IDiscordChannel>> {
         const response = await BaseApi.httpRequest.get<Array<IDiscordChannel>>(`/${discordChannelResource}/${date}/occupied`);
         return response.data;
