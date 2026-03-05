8d5203e4d6b4b0c3c668d8bd1593c19009c2be0c
Alex Barry <mrozbarry@users.noreply.github.com>
2026-03-05 09:58:55 -0500

    swapped slack listener for a subscriber

---
+++ app/Listeners/GrowthSessionSlackSubscriber.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Listeners;
+
+use App\Events\GrowthSessionAttendeeChanged;
+use App\Events\GrowthSessionCreated;
+use App\Events\GrowthSessionDeleted;
+use App\Events\GrowthSessionUpdated;
+use App\Services\Slack\SessionPoster;
+use Illuminate\Contracts\Container\BindingResolutionException;
+use Illuminate\Contracts\Container\CircularDependencyException;
+use Illuminate\Events\Dispatcher;
+use Illuminate\Http\Client\ConnectionException;
+
+class GrowthSessionSlackSubscriber
+{
+    public function __construct(protected SessionPoster $slack)
+    {
+    }
+
+    /**
+     * @throws CircularDependencyException
+     * @throws BindingResolutionException
+     * @throws ConnectionException
+     */
+    public function handleCreated(GrowthSessionCreated $event): void
+    {
+        $this->slack->post($event->growthSession);
+    }
+
+    /**
+     * @throws CircularDependencyException
+     * @throws BindingResolutionException
+     * @throws ConnectionException
+     */
+    public function handleAttendeeChanged(GrowthSessionAttendeeChanged $event): void
+    {
+        $this->slack->post($event->growthSession);
+    }
+
+    /**
+     * @throws CircularDependencyException
+     * @throws BindingResolutionException
+     * @throws ConnectionException
+     */
+    public function handleUpdated(GrowthSessionUpdated $event): void
+    {
+        $this->slack->post($event->growthSession);
+    }
+
+    public function handleDeleted(GrowthSessionDeleted $event): void
+    {
+    }
+
+    /**
+     */
+    public function subscribe(Dispatcher $dispatcher): array
+    {
+        if (!$this->slack->isConfigured()) {
+            return [];
+        }
+
+        return [
+            GrowthSessionCreated::class => 'handleCreated',
+            GrowthSessionAttendeeChanged::class => 'handleAttendeeChanged',
+            GrowthSessionUpdated::class => 'handleUpdated',
+        ];
+    }
+
+}
