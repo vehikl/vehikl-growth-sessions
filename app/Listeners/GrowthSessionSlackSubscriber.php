97b496252c82c25b81b8d191ba0808f65d2687c8
Alex Barry <mrozbarry@users.noreply.github.com>
2026-03-06 17:02:21 -0500

    Remove cooldown to posting, not necessary

--- app/Listeners/GrowthSessionSlackSubscriber.php
+++ app/Listeners/GrowthSessionSlackSubscriber.php
@@ -6,28 +6,16 @@ use App\Events\GrowthSessionAttendeeChanged;
 use App\Events\GrowthSessionCreated;
 use App\Events\GrowthSessionDeleted;
 use App\Events\GrowthSessionUpdated;
-use App\Models\GrowthSession;
 use App\Services\Slack\SessionPoster;
 use Illuminate\Contracts\Container\BindingResolutionException;
 use Illuminate\Contracts\Container\CircularDependencyException;
 use Illuminate\Events\Dispatcher;
 use Illuminate\Http\Client\ConnectionException;
-use Illuminate\Support\Facades\Cache;
 
 class GrowthSessionSlackSubscriber
 {
     public function __construct(protected SessionPoster $slack)
     {
-    }
-
-    public function cooldownLock(GrowthSession $gs): bool
-    {
-        $key = "slack:cooldown:{$gs->id}";
-        if (Cache::has($key)) {
-            return false;
-        }
-        Cache::put($key, true, now()->addSeconds(2));
-        return true;
     }
 
     /**
@@ -37,9 +25,6 @@ class GrowthSessionSlackSubscriber
      */
     public function handleCreated(GrowthSessionCreated $event): void
     {
-        if (!$this->cooldownLock($event->growthSession)) {
-            return;
-        }
         $this->slack->post($event->growthSession);
     }
 
@@ -50,10 +35,6 @@ class GrowthSessionSlackSubscriber
      */
     public function handleAttendeeChanged(GrowthSessionAttendeeChanged $event): void
     {
-        if (!$this->cooldownLock($event->growthSession)) {
-            return;
-        }
-
         $this->slack->post($event->growthSession);
     }
 
@@ -64,22 +45,12 @@ class GrowthSessionSlackSubscriber
      */
     public function handleUpdated(GrowthSessionUpdated $event): void
     {
-        if (!$this->cooldownLock($event->growthSession)) {
-            return;
-        }
-        if (in_array('slack_thread_ts', $event->dirtyKeys)) {
-            return;
-        }
-
         $this->slack->post($event->growthSession);
     }
 
     public function handleDeleted(GrowthSessionDeleted $event): void
     {
-        if (!$this->cooldownLock($event->growthSession)) {
-            return;
-        }
-
+        // TODO: Remove post
     }
 
     /**
