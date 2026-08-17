e6b569df006115aa94c186091e55fcc3af355910
Armand Kaufmann <armandkaufmann@gmail.com>
2026-08-17 16:02:40 -0400

    add broadcast route for private channel

--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Models\User;
 use Illuminate\Support\Facades\Broadcast;
 
 Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+    return (int)$user->id === (int)$id;
+});
+
+Broadcast::channel('notifications.{userId}', function (User $user, int $userId) {
+    return $user->id === $userId;
 });
